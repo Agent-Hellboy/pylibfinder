b6dcb9a16793c29cadad08f5293b8b3e7b78e6ea
Prince Roshan <princekrroshan01@gmail.com>
2023-06-03 20:23:09 +0530

    release 0.1.1

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, Extension
 
 def main():
+    with open("README.rst", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
     setup(
         name="pylibfinder",
-        version="0.1.0",
+        version="0.1.1",
         description="Library to spot a keyword as a function inside python stdlib",
+        long_description=long_description,
+        long_description_content_type="text/x-rst",
         author="Prince Roshan",
         author_email="princekrroshan01@gmail.com",
         ext_modules=[Extension("funcfinder", ["funcfinder.c"])],
